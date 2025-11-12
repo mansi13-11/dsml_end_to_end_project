34d38ef04762a27e18c20a2d436756ee88767614
mansi13-11 <mansi1998jan@gmail.com>
2025-11-12 15:05:07 +0000

    Hyperparameter tuning added

--- src/utils.py
+++ src/utils.py
@@ -7,7 +7,7 @@ from sklearn.metrics import r2_score
 import pickle
 from src.logger import logging
 from src.exception import CustomException
-
+from sklearn.model_selection import GridSearchCV
 
 def save_object(file_path: str, obj: object) -> None:
     """Saves a Python object to a file using pickle.
@@ -31,7 +31,7 @@ def save_object(file_path: str, obj: object) -> None:
         logging.error(f"Error saving object at {file_path}: {e}")
         raise CustomException(e, sys)
     
-def evaluate_model(X_train, y_train, X_test, y_test, models) -> dict:
+def evaluate_model(X_train, y_train, X_test, y_test, models, param) -> dict:
     """Evaluates multiple regression models and returns their R2 scores.
 
     Args:
@@ -53,8 +53,15 @@ def evaluate_model(X_train, y_train, X_test, y_test, models) -> dict:
         for i in range(len(list(models))):
             model_name = list(models.keys())[i]
             model = list(models.values())[i]
+            para=param[list(models.keys())[i]]
 
-            model.fit(X_train, y_train)
+            gs = GridSearchCV(model,para,cv=3)
+            gs.fit(X_train,y_train)
+
+            model.set_params(**gs.best_params_)
+            model.fit(X_train,y_train)
+
+            # model.fit(X_train, y_train)
 
             X_train_pred = model.predict(X_train)
             X_test_pred = model.predict(X_test)
